29d543d5bae6e70b7a2dd998d95f9862dedd7ab2
tuvokyang <samfresence@gmail.com>
2025-09-16 14:17:48 +0800

    fix error when use feeds url with branch

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's|^src-git luci .*|src-git luci https://github.com/TuvokYang/luci.git^luci-mod-network|' feeds.conf.default
+sed -i 's|^src-git luci .*|src-git luci https://github.com/TuvokYang/luci.git;luci-mod-network|' feeds.conf.default
 
 sed -i '/^src-link custom*/d' feeds.conf.default
 mkdir -p custom
