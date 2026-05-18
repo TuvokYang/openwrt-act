b1cb991889cbe1b0a9d12c756c1cef0a1242ba50
tuvokyang <samfresence@gmail.com>
2026-05-18 11:38:09 +0800

    fix: prevent collectd version-gen.sh from picking up parent git repo
    collectd configure runs git describe which traverses up to /workdir/openwrt/.git,
    setting LCC_VERSION_PATCH to '0-r54' (a bare identifier) causing compile failure:
      error: 'r54' undeclared
    
    Fixed by setting GIT_CEILING_DIRECTORIES to stop git traversal at collectd build dir.

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,9 @@
 
 #set llvm.download-ci-llvm=false for rust
 sed -i 's#download-ci-llvm=.* #download-ci-llvm=false #g' feeds/packages/lang/rust/Makefile
+
+# Fix collectd version-gen.sh: prevent git describe from picking up parent openwrt repo
+# Without this, LCC_VERSION_PATCH gets set to "0-r54" causing compile error
+if [ -f feeds/packages/utils/collectd/Makefile ]; then
+    echo 'MAKE_FLAGS += GIT_CEILING_DIRECTORIES="$(PKG_BUILD_DIR)/.."' >> feeds/packages/utils/collectd/Makefile
+fi
